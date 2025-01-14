97d8b370ebd4d0bb6d1d1f5cb6a261add03927cf
boss-moh <moh.saad.abu.kmail@gmail.com>
2025-01-14 15:31:52 +0200

    save

--- src/components/ShopCard/index.tsx
+++ src/components/ShopCard/index.tsx
@@ -3,13 +3,18 @@ import ProductItem from "./ProductItem";
 
 export const ShopCard = () => {
   const { products } = useShopContext();
+
   return (
     <div className="mt-2">
       <h2 className="text-2xl font-bold">Your Card</h2>
       <div className="space-y-4">
         {products.length ? (
           products.map(({ product, quantity }) => (
-            <ProductItem product={product} quantity={quantity} />
+            <ProductItem
+              key={product.id}
+              product={product}
+              quantity={quantity}
+            />
           ))
         ) : (
           <p className="p-2">Your Card Is Empty</p>
