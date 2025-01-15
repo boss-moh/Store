996fdef5029276343d32c59bee073ee8bb2a48ac
boss-moh <moh.saad.abu.kmail@gmail.com>
2025-01-15 09:42:58 +0200

    some style

--- src/components/ShopCard/index.tsx
+++ src/components/ShopCard/index.tsx
@@ -1,5 +1,6 @@
 import { ShopItem, useShopContext } from "@/context";
 import ProductItem from "./ProductItem";
+import Card from "../Card";
 
 const calcPrice = (price: number, discountPercentage: number) => {
   const discount = Number(((price * discountPercentage) / 100).toFixed(2));
@@ -33,7 +34,7 @@ export const ShopCard = () => {
       <h2 className="text-2xl font-bold">Your Card</h2>
       <div className="space-y-4">
         {products.length ? (
-          <section className="space-y-8">
+          <section className="space-y-4">
             <article className="space-y-2">
               {products.map(({ product, quantity }) => (
                 <ProductItem
@@ -44,24 +45,26 @@ export const ShopCard = () => {
               ))}
             </article>
             <article className="space-y-4">
-              <header>
-                <h3 className="text-lg font-bold">Order Summary</h3>
-              </header>
-              <article className="space-y-2">
-                <p className="flex justify-between">
-                  <span>Subtotal</span>
-                  <span className="text-red-500">${before}</span>
-                </p>
-                <p className="flex justify-between">
-                  <span> Discount</span>
-                  <span className="text-green-500">${discount}</span>
-                </p>
-                <div className=" divider" />
-                <p className="flex justify-between">
-                  <span> Total</span>
-                  <span>${after}</span>
-                </p>
-              </article>
+              <Card>
+                <header>
+                  <h3 className="text-lg font-bold">Order Summary</h3>
+                </header>
+                <article className="space-y-2">
+                  <p className="flex justify-between">
+                    <span>Subtotal</span>
+                    <span className="text-red-500">${before}</span>
+                  </p>
+                  <p className="flex justify-between">
+                    <span> Discount</span>
+                    <span className="text-green-500">${discount}</span>
+                  </p>
+                  <div className=" divider" />
+                  <p className="flex justify-between">
+                    <span> Total</span>
+                    <span>${after}</span>
+                  </p>
+                </article>
+              </Card>
               <footer>
                 <button className="w-full btn btn-primary">
                   Go For Check Out{" "}
